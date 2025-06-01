c150c74cc8e98e2c030f6fca401d757e9ece2944
G Ashwanth Badrinath <ashprime.0704@gmail.com>
2025-06-01 17:30:40 +0530

    Update navbar.jsx

--- Antenatogram-Frontend/src/components/bars/navbar.jsx
+++ Antenatogram-Frontend/src/components/bars/navbar.jsx
@@ -4,8 +4,8 @@ import useAuth from "../../hooks/useAuth";
 import { Link } from 'react-router-dom';
 import { Avatar, Dropdown } from "flowbite-react";
 import useSignOut from "../../hooks/useSignOut";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; */
+/*import { faBars } from "@fortawesome/free-solid-svg-icons";*/
 
 const Navbar = ({ toggleSidebar }) => {  // Remove async
     const { auth } = useAuth();
@@ -54,7 +54,7 @@ const Navbar = ({ toggleSidebar }) => {  // Remove async
                         Login
                     </Link>
                 )}
-                { authstatus && <FontAwesomeIcon icon={faBars} size="2x" onClick={toggleSidebar} className="block md:hidden"/>}
+{/*                  { authstatus && <FontAwesomeIcon icon={faBars} size="2x" onClick={toggleSidebar} className="block md:hidden"/>} */}
             
             </div>
         </div>
